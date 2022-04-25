33f79fcb0a26549df9516dffeabf1ab1226d5db2
Brendyn Dabrowski <>
2022-04-25 18:42:56 -0500

    Fixed font in safari.

--- src/components/Stateless/HeadingText.js
+++ src/components/Stateless/HeadingText.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const HeadingText = styled.h2`
-    font-family: Roboto;
+    font-family: "Roboto", var(--font-heading);
     text-align: center;
     margin-bottom: 5rem;
     font-size: ${props => props.fontSize};
+    font-weight: var(--fontWeight-semibold)
 `
